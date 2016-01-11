d4ed5dea09885a3d0e31bb69934c99d6fdb79f7a
Jose Romero <jr09g@my.fsu.edu>
2016-01-10 19:06:24 -0500

    correction

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -2,7 +2,7 @@ class Receipt < ActiveRecord::Base
   belongs_to :company
   belongs_to :user
   belongs_to :expense_report
-  validates :receipt_desc, :company_name, :price, :presence => true
+  validates :receipt_desc, :price, :presence => true
   has_attached_file :image
   #:styles => {:medium => "300x300>", :thumb => "100x100"}
   #validates_attachment_content_type :image, :content_type=>/\Aimage\/.*\Z/
