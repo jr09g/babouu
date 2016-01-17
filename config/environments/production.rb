6711bff78b0aa112297726fe0d398dfb05b4bfc2
Jose Romero <jr09g@my.fsu.edu>
2016-01-17 12:27:02 -0500

    another

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
-  config.paperclip_defaults = {
+    config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
