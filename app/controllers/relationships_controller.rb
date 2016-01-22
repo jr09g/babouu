3b948988f78a27078b6f282b6f885b799e1592d5
Jose Romero <jr09g@my.fsu.edu>
2016-01-21 20:57:25 -0500

    dat newnew

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -7,7 +7,7 @@ class RelationshipsController < ApplicationController
   def index
     @relationships = Relationship.all
     #
-    @users_roles = User.joins(:users_role).select("users_roles.id as ur_id users_roles.user_id as user, users.first_name as first, users.last_name as last, users.email as email, users_roles.role_id as role")
+    @users_roles = User.joins(:users_role).select("users_roles.id as ur_id, users_roles.user_id as user, users.first_name as first, users.last_name as last, users.email as email, users_roles.role_id as role")
   end
 
   # GET /relationships/1
