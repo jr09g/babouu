6c1756dc2ef706c1e7e9bcffcd2e3347aed47eac
Jose Romero <jr09g@my.fsu.edu>
2016-01-23 10:29:43 -0500

    amending role ropdown and role creation process

--- app/controllers/users_roles_controller.rb
+++ app/controllers/users_roles_controller.rb
@@ -2,7 +2,9 @@ class UsersRolesController < ApplicationController
   before_action :set_user_role, only: [:edit, :update]
   
   def edit
-  	@roles = Role.all
+  	#@roles = Role.all
+  	#instance variable that cretaes dropdown to edit employee roles; standard role omitted because it is not a valid role in business relationship scope
+  	@roles = Role.where.not(id: 1)
   end
 
   def update
