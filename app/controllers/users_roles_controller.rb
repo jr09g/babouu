05176b7120ea73a6812f4f634470d6e9230360c7
Jose Romero <jr09g@my.fsu.edu>
2016-01-23 11:06:05 -0500

    amend dropdown

--- app/controllers/users_roles_controller.rb
+++ app/controllers/users_roles_controller.rb
@@ -4,7 +4,7 @@ class UsersRolesController < ApplicationController
   def edit
   	#@roles = Role.all
   	#instance variable that cretaes dropdown to edit employee roles; standard role omitted because it is not a valid role in business relationship scope
-  	@roles = Role.where.not(id: 1)
+  	@roles = Role.where.not(id: [1,2])
   end
 
   def update
