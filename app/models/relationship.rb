3a36a6716b1473e7db13b90372eb21896f08554c
Jose Romero <jr09g@my.fsu.edu>
2016-01-28 23:08:28 -0500

    newnew

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -5,22 +5,24 @@ class Relationship < ActiveRecord::Base
   #check if relationship exists
   def self.relationship_check(user_id)
   	@user_id = user_id
-  	@relationship_find = Relationship.where(:user_id => @user_id)
+  	#@relationship_find = Relationship.where(:user_id => @user_id)
   	@does_exist
 
   	#loop through all relatioships to check if user_id already has one
-  	#Relationship.each do |relationship|
-  	#  if relationship.user_id == @user_id
-  	#  	return true
-  	#  	break
-  	#  else
-  	#  	return false
+  	Relationship.each do |relationship|
+  	  if relationship.user_id == @user_id
+  	  	@does_exist = false
+  	  	break
+  	  else
+  	  	@does_exist = true
+      end
+    end
 
-  	if @relationship_find == nil
-  	  @does_exist = true
-  	else
-  	  @does_exist = false
-  	end
+  	#if @relationship_find == nil
+  	#  @does_exist = true
+  	#else
+  	#  @does_exist = false
+  	#end
 
   	return @does_exist
 
