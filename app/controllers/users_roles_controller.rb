d0908bfa105df21b4647c1c219f3d42fe54dbc39
Jose Romero <jr09g@my.fsu.edu>
2016-01-30 17:03:14 -0500

    test adding of role name

--- app/controllers/users_roles_controller.rb
+++ app/controllers/users_roles_controller.rb
@@ -2,7 +2,7 @@ class UsersRolesController < ApplicationController
   before_action :set_user_role, only: [:edit, :update]
   
   def edit
-  	#@roles = Role.all
+  	@roles = Role.all
   	#instance variable that cretaes dropdown to edit employee roles; standard role omitted because it is not a valid role in business relationship scope
   	@roles = Role.where.not(id: [1,2])
   end
