b94565fd852fec5cf16b278765abbd08f916fa36
Jose Romero <jr09g@my.fsu.edu>
2016-01-29 21:34:04 -0500

    remove user_id column from groups model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160130013228) do
+ActiveRecord::Schema.define(version: 20160130022806) do
 
   create_table "businesses", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -61,10 +61,7 @@ ActiveRecord::Schema.define(version: 20160130013228) do
     t.integer  "manager_user_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.integer  "user_id"
   end
-
-  add_index "groups", ["user_id"], name: "index_groups_on_user_id"
 
   create_table "receipts", force: :cascade do |t|
     t.string   "receipt_desc"
