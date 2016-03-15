0e074e4a566b37c741fedd85b11c1241a769c434
Jose Romero <jr09g@my.fsu.edu>
2016-03-14 22:40:38 -0400

    amend default address for devise mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,5 +93,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #action mailer default for devise in prod
-  config.action_mailer.default_url_options = {host: 'babouu.herokuapp.com'}
+  config.action_mailer.default_url_options = {host: 'babouu.com'}
 end
