b6a9e7ff05e0af96b76371dc9f71368b797ac30e
Jose Romero <jr09g@my.fsu.edu>
2016-03-16 21:12:43 -0400

    newnew

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -5,7 +5,7 @@ class Receipt < ActiveRecord::Base
   belongs_to :user
   belongs_to :expense_report
   validates :receipt_desc, :price, :presence => true
-  #has_attached_file :image
+  has_attached_file :image
   #:styles => {:medium => "300x300>", :thumb => "100x100"}
   #validates_attachment_content_type :image, :content_type=>/\Aimage\/.*\Z/
   do_not_validate_attachment_file_type :image
