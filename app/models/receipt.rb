b3bacf74505f0284f8ac474dae63de68df04b0f4
Jose Romero <jr09g@my.fsu.edu>
2016-03-20 17:39:35 -0400

    quick

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -295,7 +295,7 @@ class Receipt < ActiveRecord::Base
 
 		#@image = RTesseract.new("../images/target_test.jpeg", :processor => "mini_magick")
 
-		@image = RTesseract.new(@file_name, :processor => "mini_magick")
+		@image = RTesseract.new(@file_name, :processor => "none")
 
 		@temp_file = Tempfile.new(['ocr', '.pdf'])
 		@temp_file.write(@image.to_s)
