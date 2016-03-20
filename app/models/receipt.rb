06fd014ce648700f38ac48c9a701c0102c0310d6
Jose Romero <jr09g@my.fsu.edu>
2016-03-20 17:32:20 -0400

    newish

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -293,9 +293,9 @@ class Receipt < ActiveRecord::Base
 	def self.manual_attachment(file_name)
 		@file_name = file_name
 
-		@image = RTesseract.new("../images/target_test.jpeg", :processor => "mini_magick")
+		#@image = RTesseract.new("../images/target_test.jpeg", :processor => "mini_magick")
 
-		#@image = RTesseract.new(@file_name, :processor => "none")
+		@image = RTesseract.new(@file_name, :processor => "mini_magick")
 
 		@temp_file = Tempfile.new(['ocr', '.pdf'])
 		@temp_file.write(@image.to_s)
