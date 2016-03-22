a32d785d70401e6bd8367010b6d4f452376f2219
Jose Romero <jr09g@my.fsu.edu>
2016-03-21 22:41:24 -0400

    ok seriously last one

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -299,8 +299,8 @@ class Receipt < ActiveRecord::Base
 		#@image = RTesseract.new(@file_name, :processor => "none")
 		#@image.to_s
 
-		@to_tiff = system('convert -density 300 ' + @file_name + ' -depth 8 ' + @file_name.sub(/\.[^.]+\z/, ".tiff"))
-		@to_txt = system('tesseract ' + @file_name + ' ' + File.basename( @file_name, ".*" ))
+		@to_tiff = exec('convert -density 300 ' + @file_name + ' -depth 8 ' + @file_name.sub(/\.[^.]+\z/, ".tiff"))
+		@to_txt = exec('tesseract ' + @file_name + ' ' + File.basename( @file_name, ".*" ))
 
 		#@test = system('vi ' + @file_name.sub(/\.[^.]+\z/, ".txt"))
 		@test = `vi #{@file_name.sub(/\.[^.]+\z/, ".txt")}`
