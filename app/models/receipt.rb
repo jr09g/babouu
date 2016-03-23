ab20eb08670149930c829dc0e4eff5be86eafc34
Jose Romero <jr09g@my.fsu.edu>
2016-03-23 19:41:58 -0400

    newnew

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -305,7 +305,7 @@ class Receipt < ActiveRecord::Base
 
 		#@test = system('vi ' + @file_name.sub(/\.[^.]+\z/, ".txt"))
 		#@test = `#{vi "#{@file_name.sub(/\.[^.]+\z/, ".txt")}"}`
-		@image = RTesseract.new(@file_name.path, :processor => "mini_magick")
+		@image = RTesseract.new(Rails.root.join(@file_name.path), :processor => "none")
 
 		@temp_file = Tempfile.new(['ocr', '.txt'])
 		@temp_file.write(@image.to_s)
