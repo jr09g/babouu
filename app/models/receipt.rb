8c53d4c7d5bf5a77e2087e90e1ec43c54c6c0555
Jose Romero <jr09g@my.fsu.edu>
2016-03-24 21:39:44 -0400

    another new one

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -298,8 +298,7 @@ class Receipt < ActiveRecord::Base
 		#@image.to_s
 
 		#@to_tiff = system('convert -density 300 ' + @file_name + ' -depth 8 ' + @file_name.sub(/\.[^.]+\z/, ".tiff"))
-		@initial = system('cd ~')
-		@to_txt = system('tesseract ' + @file_name + ' ' + @file_name.sub(/\.[^.]+\z/, ""))
+		@to_txt = system('tesseract ' + @file_name + ' result')
 
 		#@to_tiff = `#{convert "-density 300" "#{@file_name}" depth "8" "#{@file_name.sub(/\.[^.]+\z/, ".txt")}"}`
 		#@to_txt = `#{tesseract "#{@file_name.sub(/\.[^.]+\z/, ".tiff")}" "#{@file_name.sub(/\.[^.]+\z/, "")}"}`
