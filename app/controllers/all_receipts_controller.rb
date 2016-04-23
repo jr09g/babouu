7fc20bfebd065d569bd84870df29c47ff85cf7fe
Jose Romero <jr09g@my.fsu.edu>
2016-04-22 23:23:30 -0400

    expand to all receipts page

--- app/controllers/all_receipts_controller.rb
+++ app/controllers/all_receipts_controller.rb
@@ -2,8 +2,12 @@ class AllReceiptsController < ApplicationController
   def index
   	#instance variable to retrieve all records from the user table
 	@users = User.all
+	#instance variable to retrieve all records from the receipt table; joined with expense_report table to bring view name in
+    @receipt_with_view = Receipt.joins(:expense_report).select("receipts.id, receipts.user_id, receipts.company_name, receipts.receipt_desc, receipts.price, expense_reports.name as name, receipts.plain_date, receipts.created_at")
 	#instance variable to retrieve all records from the receipt table in descending order
 	@receipts = Receipt.order(created_at: :desc)
+	#
+	@receipts = @receipt_with_view.order(created_at: :desc)
 	#insacne variable to group dates for receipts so that all receipts can be sectioned off into those dates
 	@receipt_dates = Receipt.select("plain_date, sum(price) as price_by_date").group("plain_date").order(plain_date: :desc)
   end
