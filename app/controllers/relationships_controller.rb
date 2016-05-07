48f9a3c9f2bb433adb43761239be3655c989db47
Jose Romero <jr09g@my.fsu.edu>
2016-05-07 14:30:52 -0400

    amend join structure

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -8,14 +8,11 @@ class RelationshipsController < ApplicationController
   def index
     @relationships = Relationship.all
     #
-    #@users_roles_pre = User.joins(:users_role)#.select("users_roles.id as ur_id, users_roles.user_id as user, users.first_name as first, users.last_name as last, users.email as email, users_roles.role_id as role")
+    @users_roles_pre = User.joins(:users_role)#.select("users_roles.id as ur_id, users_roles.user_id as user, users.first_name as first, users.last_name as last, users.email as email, users_roles.role_id as role")
     #TEST LEAVE FOR LATER
-    #@users_roles = @users_roles_pre.joins(:role).select("users_roles.id as ur_id, users.id as user, users.first_name as first, users.last_name as last, users.email as email, roles.name as role")
+    @users_roles = @users_roles_pre.joins('INNER JOIN roles on roles.id = users_roles.role_id').select("users_roles.id as ur_id, users.id as user, users.first_name as first, users.last_name as last, users.email as email, roles.name as role")
     #TEST QUERY
     #@users_roles_query = UsersRole.joins('INNER JOIN users ON users.id = users_roles.user_id INNER JOIN roles ON roles.id = users_roles.role_id')
-    @users_roles_query = UsersRole.joins(:user, :role)
-    #TEST SELECT
-    @users_roles = @users_roles_query.select("users_roles.id as ur_id, users.id as user, users.first_name as first, users.last_name as last, users.email as email, roles.name as role")
   end
 
   # GET /relationships/1
